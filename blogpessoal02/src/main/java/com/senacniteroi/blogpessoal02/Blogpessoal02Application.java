19d07d962394ecb75bc84842222f1180d5e9adc3
teste_git <113646572+AnaniasDaSilva@users.noreply.github.com>
2023-02-24 12:08:32 -0300

    Add files via upload

---
+++ blogpessoal02/src/main/java/com/senacniteroi/blogpessoal02/Blogpessoal02Application.java
@@ -0,0 +1,13 @@
+package com.senacniteroi.blogpessoal02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Blogpessoal02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Blogpessoal02Application.class, args);
+	}
+
+}
---
+++ blogpessoal02/src/test/java/com/senacniteroi/blogpessoal02/Blogpessoal02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senacniteroi.blogpessoal02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Blogpessoal02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
